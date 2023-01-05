99f6e585fd11e1d63c907217d63465f992157b62
Adam Sokołowski <adam.sokolowski@europcar.com>
2023-01-05 22:00:46 +0100

    docs(sentenceCase): adds function description

--- src/sentenceCase.ts
+++ src/sentenceCase.ts
@@ -6,6 +6,19 @@ import startCase from 'lodash/startCase';
 import toLower from 'lodash/toLower';
 import upperFirst from 'lodash/upperFirst';
 
+/**
+ * Transforms a string into a `Sentence case` - capitalizes first letter of the string.
+ * @param value string
+ * @returns string
+ *
+ * @example
+ * 'myString' => 'My string'
+ * 'my_string' => 'My string'
+ * 'MY_STRING' => 'My string'
+ * 'my string' => 'My string'
+ * 'My string' => 'My string'
+ * 'My String' => 'My string'
+ */
 export function sentenceCase(value: string): string {
   if(isString(value) && !isNaN(parseInt(value))) {
     return value;
